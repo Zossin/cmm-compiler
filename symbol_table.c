696d18692e9ce839bb3412dfebdc000df86f9ee3
Zossin <njulzx@gmail.com>
2012-04-24 19:49:21 +0800

    type match

--- symbol_table.c
+++ symbol_table.c
@@ -18,6 +18,27 @@ void print_error(int type, int lineno, char *tips) {
     fprintf(stdout, "Error type %d at line %d: %s\n", type, lineno, tips); 
 }
 
+unsigned char is_same_type(Type *a, Type *b) {
+    if (a->kind != b->kind)
+        return FALSE;
+    if (a->kind == Basic)
+        return a->u.basic == b->u.basic;
+    else if (a->kind == Array)
+        return is_same_type(a->u.array.elem, b->u.array.elem);
+    else { //Structure
+        FieldList *a_field = a->u.structure, *b_field = b->u.structure;
+        while (a_field && b_field) {
+            if (!is_same_type(a_field->type, b_field->type))
+                return FALSE;
+            a_field = a_field->next;
+            b_field = b_field->next;
+        }
+        if (a_field || b_field)
+            return FALSE;
+        return TRUE;
+    }
+}
+
 symbol_node *get_symbol(char *name) {
     int index = hash_pjw(name);
     hash_node *p_node = symbol_table[index].head;
@@ -37,6 +58,25 @@ void insert_symbol(symbol_node *p_symbol) {
     new_node->data = *p_symbol;
     new_node->next = symbol_table[index].head;
     symbol_table[index].head = new_node;
+}
+
+extern void sdt(syntax_tree_node *p_node);
+
+void check_type_match(syntax_tree_node *p_node, syntax_tree_node **children) {
+    sdt(children[0]);
+    sdt(children[2]);
+    if (children[0]->attr.is_legal && children[2]->attr.is_legal) {
+        if (is_same_type(children[0]->attr.type, children[2]->attr.type)) {
+            p_node->attr.type = children[0]->attr.type;
+            p_node->attr.is_legal = TRUE;
+        }
+        else {
+            print_error(7, children[1]->lineno, "Type mismatched");
+            p_node->attr.is_legal = FALSE;
+        }
+    }
+    else
+        p_node->attr.is_legal = FALSE;
 }
 
 #define NR_CHILDREN 8
@@ -70,72 +110,108 @@ void sdt(syntax_tree_node *p_node) {
         }
     }
     else if (p_node->type == Def_SYNTAX) {
-
+        if (child_num == 3 && children[0]->type == Specifier_SYNTAX && children[1]->type == DecList_SYNTAX && children[2]->type == SEMI_TOKEN) {
+            sdt(children[0]);
+            children[1]->attr.inh_type = children[0]->attr.type;
+            sdt(children[1]);
+        }
+    }
+    else if (p_node->type == DecList_SYNTAX) {
+        if (child_num == 1 && children[0]->type == Dec_SYNTAX) {
+            children[0]->attr.inh_type = p_node->attr.inh_type;
+            sdt(children[0]);
+        }
+        else if (child_num == 3 && children[0]->type == Dec_SYNTAX && children[1]->type == COMMA_TOKEN && children[2]->type == DecList_SYNTAX) {
+            children[0]->attr.inh_type = p_node->attr.inh_type;
+            sdt(children[0]);
+            children[2]->attr.inh_type = p_node->attr.inh_type;
+            sdt(children[2]);
+        }
+    }
+    else if (p_node->type == Dec_SYNTAX) {
+        if (child_num == 1 && children[0]->type == VarDec_SYNTAX) {
+            children[0]->attr.inh_type = p_node->attr.inh_type;
+            sdt(children[0]);
+        }
+        else if (child_num == 3 && children[0]->type == VarDec_SYNTAX && children[1]->type == ASSIGNOP_TOKEN && children[2]->type == Exp_SYNTAX) {
+            children[0]->attr.inh_type = p_node->attr.inh_type;
+            sdt(children[0]);
+            sdt(children[2]);
+            if (children[2]->attr.is_legal) {
+                if (is_same_type(children[0]->attr.type, children[2]->attr.type)) 
+                    p_node->attr.type = children[0]->attr.type;
+                else 
+                    print_error(5, children[1]->lineno, "Type mismatched");
+            }
+        }
     }
     else if (p_node->type == Exp_SYNTAX) {
         if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == ASSIGNOP_TOKEN && children[2]->type == Exp_SYNTAX) {
             sdt(children[0]);
-            sdt(children[1]);
             sdt(children[2]);
+            if (children[0]->attr.is_legal && children[2]->attr.is_legal) {
+                if (is_same_type(children[0]->attr.type, children[2]->attr.type)) {
+                    p_node->attr.type = children[0]->attr.type;
+                    p_node->attr.is_legal = TRUE;
+                }
+                else {
+                    print_error(5, children[1]->lineno, "Type mismatched");
+                    p_node->attr.is_legal = FALSE;
+                }
+            }
+            else
+                p_node->attr.is_legal = FALSE;
         }
         else if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == AND_TOKEN && children[2]->type == Exp_SYNTAX) {
-            sdt(children[0]);
-            sdt(children[1]);
-            sdt(children[2]);
+            check_type_match(p_node, children);
         }
         else if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == OR_TOKEN && children[2]->type == Exp_SYNTAX) {
-            sdt(children[0]);
-            sdt(children[1]);
-            sdt(children[2]);
+            check_type_match(p_node, children);
         }
         else if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == RELOP_TOKEN && children[2]->type == Exp_SYNTAX) {
-            sdt(children[0]);
-            sdt(children[1]);
-            sdt(children[2]);
+            check_type_match(p_node, children);
         }
         else if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == PLUS_TOKEN && children[2]->type == Exp_SYNTAX) {
-            sdt(children[0]);
-            sdt(children[1]);
-            sdt(children[2]);
+            check_type_match(p_node, children);
         }
         else if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == MINUS_TOKEN && children[2]->type == Exp_SYNTAX) {
-            sdt(children[0]);
-            sdt(children[1]);
-            sdt(children[2]);
+            check_type_match(p_node, children);
         }
         else if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == STAR_TOKEN && children[2]->type == Exp_SYNTAX) {
-            sdt(children[0]);
-            sdt(children[1]);
-            sdt(children[2]);
+            check_type_match(p_node, children);
         }
         else if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == DIV_TOKEN && children[2]->type == Exp_SYNTAX) {
-            sdt(children[0]);
-            sdt(children[1]);
-            sdt(children[2]);
+            check_type_match(p_node, children);
         }
         else if (child_num == 3 && children[0]->type == LP_TOKEN && children[1]->type == Exp_SYNTAX && children[2]->type == RP_TOKEN) {
-            sdt(children[0]);
             sdt(children[1]);
-            sdt(children[2]);
+            p_node->attr.is_legal = children[1]->attr.is_legal;
+            p_node->attr.type = children[1]->attr.type;
         }
         else if (child_num == 2 && children[0]->type == MINUS_TOKEN && children[1]->type == Exp_SYNTAX) {
-            sdt(children[0]);
             sdt(children[1]);
+            p_node->attr.is_legal = children[1]->attr.is_legal;
+            p_node->attr.type = children[1]->attr.type;
         }
         else if (child_num == 2 && children[0]->type == NOT_TOKEN && children[1]->type == Exp_SYNTAX) {
-            sdt(children[0]);
             sdt(children[1]);
+            p_node->attr.is_legal = children[1]->attr.is_legal;
+            p_node->attr.type = children[1]->attr.type;
         }
-        else if (child_num == 4 && children[0]->type == ID_TOKEN && children[1]->type == LP_TOKEN && children[2]->type == Args_SYNTAX && children[3]->type == RP_TOKEN) {
-            if (get_symbol(children[0]->value.str_val) == NULL) {
-                char tmp_str[100];
-                strcpy(tmp_str, "Undefined function \"");
-                print_error(2, children[0]->lineno, strcat(strcat(tmp_str, children[0]->value.str_val), "\""));
-                p_node->attr.is_legal = FALSE;
+        else if (child_num == 4 && children[0]->type == ID_TOKEN && children[1]->type == LP_TOKEN && children[2]->type == Args_SYNTAX && children[3]->type == RP_TOKEN || child_num == 3 && children[0]->type == ID_TOKEN && children[1]->type == LP_TOKEN && children[2]->type == RP_TOKEN) {
+            syntax_tree_node *args = (child_num == 4) ? children[2] : NULL;
+            symbol_node *func_node;
+            if (func_node = get_symbol(children[0]->value.str_val)) {
+                if (func_node->type == Var) {
+                    print_error(11, children[0]->lineno, "Use a variable as function.");
+                    p_node->attr.is_legal = FALSE;
+                }
+                else  {
+                    p_node->attr.is_legal = TRUE;
+                    p_node->attr.type = func_node->u.func_val.ret_type;
+                }
             }
-        }
-        else if (child_num == 3 && children[0]->type == ID_TOKEN && children[1]->type == LP_TOKEN && children[2]->type == RP_TOKEN) {
-            if (get_symbol(children[0]->value.str_val) == NULL) {
+            else {
                 char tmp_str[100];
                 strcpy(tmp_str, "Undefined function \"");
                 print_error(2, children[0]->lineno, strcat(strcat(tmp_str, children[0]->value.str_val), "\""));
@@ -144,17 +220,40 @@ void sdt(syntax_tree_node *p_node) {
         }
         else if (child_num == 4 && children[0]->type == Exp_SYNTAX && children[1]->type == LB_TOKEN && children[2]->type == Exp_SYNTAX && children[3]->type == RB_TOKEN) {
             sdt(children[0]);
-            sdt(children[1]);
+            if (!children[0]->attr.is_legal) {
+                p_node->attr.is_legal = FALSE;
+                return;
+            }
+            if (children[0]->attr.type->kind != Array) {
+                print_error(10, children[0]->lineno, "Use a not-array variable as array");
+                p_node->attr.is_legal = FALSE;
+                return;
+            }
             sdt(children[2]);
-            sdt(children[3]);
+            if (!children[2]->attr.is_legal) {
+                p_node->attr.is_legal = FALSE;
+                return;
+            }
+            if (children[2]->attr.type->kind == Basic && children[2]->attr.type->u.basic == Int) {
+                p_node->attr.type = children[0]->attr.type->u.array.elem;
+                p_node->attr.is_legal = TRUE;
+            }
+            else {
+                print_error(12, children[2]->lineno, "Array index is not integer");
+                p_node->attr.is_legal = FALSE;
+            }
         }
         else if (child_num == 3 && children[0]->type == Exp_SYNTAX && children[1]->type == DOT_TOKEN && children[2]->type == ID_TOKEN) {
             sdt(children[0]);
-            sdt(children[1]);
             sdt(children[2]);
         }
         else if (child_num == 1 && children[0]->type == ID_TOKEN) {
-            if (get_symbol(children[0]->value.str_val) == NULL) {
+            symbol_node *var_node;
+            if (var_node = get_symbol(children[0]->value.str_val)) {
+                p_node->attr.type = var_node->u.var_val.type;
+                p_node->attr.is_legal = TRUE;
+            }
+            else {
                 char tmp_str[100];
                 strcpy(tmp_str, "Undefined variable \"");
                 print_error(1, children[0]->lineno, strcat(strcat(tmp_str, children[0]->value.str_val), "\""));
@@ -162,17 +261,23 @@ void sdt(syntax_tree_node *p_node) {
             }
         }
         else if (child_num == 1 && children[0]->type == INT_TOKEN) {
-            sdt(children[0]);
+            p_node->attr.type = (Type*)malloc(sizeof(Type));
+            p_node->attr.type->kind = Basic;
+            p_node->attr.type->u.basic = Int;
+            p_node->attr.is_legal = TRUE;
         }
         else if (child_num == 1 && children[0]->type == FLOAT_TOKEN) {
-            sdt(children[0]);
+            p_node->attr.type = (Type*)malloc(sizeof(Type));
+            p_node->attr.type->kind = Basic;
+            p_node->attr.type->u.basic = Float;
+            p_node->attr.is_legal = TRUE;
         }
     }
     else if (p_node->type == Specifier_SYNTAX) {
         if (child_num == 1 && children[0]->type == TYPE_TOKEN) {
             p_node->attr.type = (Type*)malloc(sizeof(Type));
             p_node->attr.type->kind = Basic;
-            p_node->attr.type->u.basic == children[0]->value.type_val;
+            p_node->attr.type->u.basic = children[0]->value.type_val;
         }
         else if (child_num == 1 && children[0]->type == StructSpecifier_SYNTAX) {
             p_node->attr.type = children[0]->attr.type;
@@ -225,6 +330,7 @@ void sdt(syntax_tree_node *p_node) {
             else {
                 print_error(3, children[0]->lineno, "Variable redefined.");
             }
+            p_node->attr.type = p_node->attr.inh_type;
         }
         else if (child_num == 4 && children[0]->type == VarDec_SYNTAX && children[1]->type == LB_TOKEN && children[2]->type == INT_TOKEN && children[3]->type == RB_TOKEN) {
             Type *type_node = (Type*)malloc(sizeof(Type));
@@ -233,6 +339,7 @@ void sdt(syntax_tree_node *p_node) {
             type_node->u.array.size = children[2]->value.int_val;
             children[0]->attr.inh_type = type_node;
             sdt(children[0]);
+            p_node->attr.type = children[0]->attr.type;
         }
     }
     else if (p_node->type == FunDec_SYNTAX) {
